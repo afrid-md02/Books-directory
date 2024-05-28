defa6aca6259721067dc18d49f9a389ca38f75b0
afrid.md <165661014+afrid-md02@users.noreply.github.com>
2024-05-28 23:36:48 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+ // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    open: true,
+  },
+});
+
